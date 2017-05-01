e98cd511758e434f060a563b7ebef03f2c59792b
holmes0078 <tusharahuja2@gmail.com>
2017-04-30 22:42:37 -0500

    Added Pyspark Code for Predicting Actual vs Predicted Number of Trips

---
+++ logistic_regression.py
@@ -0,0 +1,28 @@
+from pyspark.ml.regression import LinearRegression
+
+# Load training data
+training = spark.read.format("libsvm")\
+.load("/home/ta1302/Citi-Bike-Analysis/citi_weather_libsvm.txt")
+
+(train,test) = training.randomSplit([0.8,0.2])
+
+lr = LinearRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8)
+
+# Fit the model
+lrModel = lr.fit(train)
+
+predictions = lrModel.transform(test)
+
+# Print the coefficients and intercept for linear regression
+print("Coefficients: %s" % str(lrModel.coefficients))
+print("Intercept: %s" % str(lrModel.intercept))
+
+# Summarize the model over the training set and print out some metrics
+trainingSummary = lrModel.summary
+print("numIterations: %d" % trainingSummary.totalIterations)
+print("objectiveHistory: %s" % str(trainingSummary.objectiveHistory))
+trainingSummary.residuals.show()
+print("RMSE: %f" % trainingSummary.rootMeanSquaredError)
+print("r2: %f" % trainingSummary.r2)
+
+predictions.toPandas().toCSV('Predictions.csv')
